582409350b76230b35be6a0fff1be0191120b38c
Janu Sung <janu.m.sung@gmail.com>
2020-02-22 08:51:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet(array)
   array.map {|word| 
     word_array = word.split('')
-    word_array[0] = word_array[0].upcase!
+    word_array[0] = word_array[0].upcase
     word_array << "!"
     word_array.join('')
   }
